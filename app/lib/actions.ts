dca2dd379609039bd028890eb2d52468a7f71884
legionOfGo <folayanjoey@gmail.com>
2024-12-22 08:20:16 +0100

    y

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 import { z } from 'zod';
-import { createClient, sql } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
